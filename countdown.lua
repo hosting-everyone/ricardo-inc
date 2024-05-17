431a96c9bf4fcbb2d210b84aea4c7ad1289f0b72
Hosting <163719152+hosting-everyone@users.noreply.github.com>
2024-05-17 11:22:59 +0200

    Update countdown.lua

--- countdown.lua
+++ countdown.lua
@@ -1,7 +1,8 @@
-countdown/
+ricardo-inc/
 │
 ├── countdown/
 │   └── index.html         <-- Pagina met de countdown
 │
 └── website/
-    └── index.html  <-- Pagina na afloop van de countdown 
+    └── index.html         <-- Pagina na afloop van de countdown
+
