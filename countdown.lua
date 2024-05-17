c4e63965d7fdff779477dd3c92a47f4c0a6ae2c5
Hosting <163719152+hosting-everyone@users.noreply.github.com>
2024-05-17 11:09:16 +0200

    Create countdown.lua

---
+++ countdown.lua
@@ -0,0 +1,9 @@
+project_folder/
+│
+├── coming_soon/
+│   ├── index.html         <-- Pagina met de countdown
+│   └── style.css          <-- Stijlblad voor de countdown pagina
+│
+└── website/
+    ├── index.html         <-- Pagina na afloop van de countdown
+    └── style.css          <-- Stijlblad voor de hoofdpagina
